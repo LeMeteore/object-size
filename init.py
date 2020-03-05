89a8a9a2abe12aac60d791a07643f509b117068e
Nsukami Di Kiesse Patrick <ptrck@nskm.xyz>
2020-03-05 03:04:32 +0000

    tiny modifications

--- init.py
+++ init.py
@@ -1,6 +1,6 @@
 """
 Filename: init.py
-Usage: This script will measure different objects in the frame using a reference object of known dimension. 
+Usage: This script will measure different objects in the frame using a reference object of known dimension.
 The object with known dimension must be the leftmost object.
 Author: Shashank Sharma
 """
@@ -13,22 +13,24 @@ import cv2
 
 # Function to show array of images (intermediate results)
 def show_images(images):
-	for i, img in enumerate(images):
-		cv2.imshow("image_" + str(i), img)
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
+        for i, img in enumerate(images):
+                cv2.imshow("image_" + str(i), img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
-img_path = "images/example_02.jpg"
+img_path = "images/foo01.jpg"
 
 # Read image and preprocess
 image = cv2.imread(img_path)
 
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blur = cv2.GaussianBlur(gray, (9, 9), 0)
+cv2.imwrite('images/foo01_blurred.jpg', blur)
 
 edged = cv2.Canny(blur, 50, 100)
 edged = cv2.dilate(edged, None, iterations=1)
 edged = cv2.erode(edged, None, iterations=1)
+cv2.imwrite('images/foo01_edged.jpg', edged)
 
 #show_images([blur, edged])
 
@@ -61,19 +63,20 @@ pixel_per_cm = dist_in_pixel/dist_in_cm
 
 # Draw remaining contours
 for cnt in cnts:
-	box = cv2.minAreaRect(cnt)
-	box = cv2.boxPoints(box)
-	box = np.array(box, dtype="int")
-	box = perspective.order_points(box)
-	(tl, tr, br, bl) = box
-	cv2.drawContours(image, [box.astype("int")], -1, (0, 0, 255), 2)
-	mid_pt_horizontal = (tl[0] + int(abs(tr[0] - tl[0])/2), tl[1] + int(abs(tr[1] - tl[1])/2))
-	mid_pt_verticle = (tr[0] + int(abs(tr[0] - br[0])/2), tr[1] + int(abs(tr[1] - br[1])/2))
-	wid = euclidean(tl, tr)/pixel_per_cm
-	ht = euclidean(tr, br)/pixel_per_cm
-	cv2.putText(image, "{:.1f}cm".format(wid), (int(mid_pt_horizontal[0] - 15), int(mid_pt_horizontal[1] - 10)), 
-		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 2)
-	cv2.putText(image, "{:.1f}cm".format(ht), (int(mid_pt_verticle[0] + 10), int(mid_pt_verticle[1])), 
-		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 2)
+        box = cv2.minAreaRect(cnt)
+        box = cv2.boxPoints(box)
+        box = np.array(box, dtype="int")
+        box = perspective.order_points(box)
+        (tl, tr, br, bl) = box
+        cv2.drawContours(image, [box.astype("int")], -1, (0, 0, 255), 2)
+        mid_pt_horizontal = (tl[0] + int(abs(tr[0] - tl[0])/2), tl[1] + int(abs(tr[1] - tl[1])/2))
+        mid_pt_verticle = (tr[0] + int(abs(tr[0] - br[0])/2), tr[1] + int(abs(tr[1] - br[1])/2))
+        wid = euclidean(tl, tr)/pixel_per_cm
+        ht = euclidean(tr, br)/pixel_per_cm
+        cv2.putText(image, "{:.1f}cm".format(wid), (int(mid_pt_horizontal[0] - 15), int(mid_pt_horizontal[1] - 10)),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 2)
+        cv2.putText(image, "{:.1f}cm".format(ht), (int(mid_pt_verticle[0] + 10), int(mid_pt_verticle[1])),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 2)
 
-show_images([image])
+cv2.imwrite('images/foo01rslt.jpg', image)
+# show_images([image])
